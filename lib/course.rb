facc0bcca3c5b0a8e0dda359177fb1bf0f946e38
Anup Khadka <akhadka.dev@gmail.com>
2019-01-13 17:26:47 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
 
+  # class methods
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+end
