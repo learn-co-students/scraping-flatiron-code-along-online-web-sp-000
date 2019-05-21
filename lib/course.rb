17cafa1be616049f4c112b73f0b1bb70f63f3656
Ramon Chavez <rachavez1974@gmail.com>
2019-05-21 16:29:12 -0400

    Finish scraping-flatiron-code-along-online-web-sp-000

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,22 @@
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all = []
 
+  def initialize
+    save << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    all.clear
+  end
+
+  private
+    def save
+      self.class.all
+    end
+end
 
