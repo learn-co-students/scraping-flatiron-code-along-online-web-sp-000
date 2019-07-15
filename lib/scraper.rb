a232ced8d0bb8aae0904e84107c6fe03ff03a387
juan siu <juan.siu@gmail.com>
2019-07-15 16:52:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,15 +23,16 @@ class Scraper
   end
   
   def make_courses
-  self.get_courses.each do |post|
+    self.get_courses.each do |post|
     course = Course.new
     course.title = post.css("h2").text
     course.schedule = post.css(".date").text
     course.description = post.css("p").text
+    end
   end
+
 end
-  
-  
-end
+
+Scraper.new.print_courses
 
 
