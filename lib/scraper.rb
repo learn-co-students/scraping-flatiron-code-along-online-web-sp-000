37a1a38a6436bdb604273d1018b3f18ef2e6214f
Blythe Pollard <blytheepollard@gmail.com>
2019-10-16 00:54:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  def get_page #responsible for getting the code in the HTML document
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  
+  def get_courses #operate on HTML page (the return value of #get_page) and return collection of elements for each course
+    self.get_page.css(".post")
+  end
+  
+  def make_courses
+     self.get_courses.each do |post| #for each iteration of elements returned by doc.css(".post") line, we are making a new instance of course and giving it a title, schedule, description
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
   
   def print_courses
     self.make_courses
@@ -18,5 +36,6 @@ class Scraper
   
 end
 
-
+Scraper.new.get_page
+Scraper.new.print_courses
 
