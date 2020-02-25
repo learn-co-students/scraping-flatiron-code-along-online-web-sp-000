777af64ca5e3d6c934b156e85a745cc1c08a10d0
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-25 03:30:38 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,28 @@ require_relative './course.rb'
 
 class Scraper
   
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  
+  def get_courses
+    self.get_page.css(".post")
+  end 
+  
+  def make_courses
+    self.get_courses.each do |article|
+      course = Course.new
+      course.title = article.css("h2").text.strip
+      course.schedule = article.css("date").text.strip
+      course.description = article.css("p").text.strip
+    end
+  end 
+      
+
+
+  
+  
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,7 +38,5 @@ class Scraper
     end
   end
   
-end
-
-
+end 
 
