da32080f606205ac85f1922f0290ac7761d646ed
shacoca <shacoca@yahoo.com>
2020-10-19 01:19:59 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,50 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
+  end
+    
   def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+      self.make_courses
+      Course.all.each do |course|
+        if course.title && course.title != ""
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
       end
+  end
+
+  def get_courses
+    #using a CSS selector to grab all of the HTML elements that contain a course
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    #instantiating `Course` objects and giving each course object the correct `title`, `schedule` and `description` attribute 
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
   end
   
 end
 
 
+Scraper.new.print_courses
 
