795a8e3534eeb2169812c2a3c0b48c9fa6104f4d
Chris Houghton <chrishoughton2434@gmail.com>
2020-10-26 14:22:47 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,22 @@
 
+class Course
+  
+  attr_accessor :title, :schedule, :description
+  
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
+  
+  # Nokogiri::HTML(open(" http://learn-co-curriculum.github.io/site-for-scraping/courses."))
+end
 
