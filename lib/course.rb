e52187c0c21c6500d2c48d5c45f648f352b1ca7e
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-23 18:21:41 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,22 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
 
+  def initialize
+    save
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+end
