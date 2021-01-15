294cbe6c8fe69c36639a5176715ecac1a14d0006
Jordan Reed <jordanreed03@gmail.com>
2021-01-15 20:30:15 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
-
+class Course
+  attr_accessor :title, :schedule, :description, :all
+  @@all = []
+  
+  def initialize
+    @title
+    @@all << self 
+  end 
+  
+  def self.reset_all
+     @@all.clear
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+end
 
