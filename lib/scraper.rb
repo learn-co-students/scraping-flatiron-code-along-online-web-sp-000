032928005dac128241ec2e1ac58ff9d93afa4028
Rosie Choi <rosiechoimanso@gmail.com>
2021-01-17 22:03:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,11 +10,11 @@ class Scraper
       Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     end
 
-    def get_courses
+    def get_courses #return the collection of Nokogiri XML elements that describe each course.
       self.get_page.css(".post")
     end
 
-    def make_courses
+    def make_courses #iterate over the collection and make a new instance of Course class for each one while assigning it the appropriate attributes
       self.get_courses.each do |post|
         course = Course.new
         course.title = post.css("h2").text
