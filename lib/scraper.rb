80594c9d340881a911d8d4c65e0600c3e2bb8e7a
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-21 22:46:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,15 +8,17 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-     L::Docu doc = scraper.get_page
     html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses') 
-    doc = Nokogiri::HTML("http://learn-co-curriculum.github.io/site-for-scraping/courses")  
+    #this holds the html code in this variable called html
+    doc = Nokogiri::HTML(html)  
+    #this turns the html into nodes
+    
   end
   
   
   def get_courses 
      doc = scraper.get_page 
-    doc.css("")
+    doc.css("<h2>NYC Web Development Fellowship</h2>")
      course_offerings.each do |course_offering|
         expect(course_offering).to be_a(Nokogiri::XML::Element)
         expect{course_offering.css("h2").text}
@@ -29,7 +31,7 @@ class Scraper
  
   def print_courses
     self.make_courses
-    Course.all.each do |course|
+    Course.all.each do |course| 
       if course.title && course.title != ""
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
@@ -40,6 +42,4 @@ class Scraper
 end
 
 #Scraper.new.get_page
-
-
- 
+end 
