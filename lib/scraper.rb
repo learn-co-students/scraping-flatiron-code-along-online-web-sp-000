e90b79eedfc16c8ff19ee821c0af997a69ab62c7
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-22 02:00:06 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,38 +8,45 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-    html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses') 
+    #html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses') 
     #this holds the html code in this variable called html
-    doc = Nokogiri::HTML(html)  
-    #this turns the html into nodes
     
+    doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses')) 
+    
+    #this turns the html into nodes 
+     
+     doc.css(".post").each do |post|
+       course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+      binding.pry 
+      
+    end
+  end
   end
   
   
-  def get_courses 
-     doc = scraper.get_page 
-    doc.css("<h2>NYC Web Development Fellowship</h2>")
-     course_offerings.each do |course_offering|
-        expect(course_offering).to be_a(Nokogiri::XML::Element)
-        expect{course_offering.css("h2").text}
-  end
+  # def get_courses 
   
+#   doc.css(".post").first.css("h2").text
+# => "Web Development Immersive"
+# doc.css(".post").first.css(".date").text
+# => "Full-Time"
+# doc.css(".post").first.css("p").text
+# => "An intensive, Ruby and Javascript course that teaches the skills necessary to start a career as a full-stack software developer."
 
-  def make_courses
-  end 
-  
- 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course| 
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end 
+ def make_courses
+   
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course| 
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+   # end 
   end
-end
 
-#Scraper.new.get_page
-end 
+Scraper.new.get_page
