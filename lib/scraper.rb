0792b3415b2178c9dffce44286126d5752a9c921
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-22 18:04:11 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,45 +8,64 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-    #html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses') 
+    html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses') 
     #this holds the html code in this variable called html
     
     doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses')) 
     
     #this turns the html into nodes 
      
-     doc.css(".post").each do |post|
-       course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-      binding.pry 
+    # doc.css(".post").each do |post|
+    #   #we use a dot before post because this is a class name 
+      course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
       
     end
   end
+  
+  
+  def get_courses 
+    self.get_page 
+    self.get_page.css(".post")
+  end 
+  
+ 
+ def make_courses
+   self.get_courses.each do |post| 
+    course = Course.new
+    #do not need to establish that course is a [] because this is created in the course file.
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+   course.description = post.css("p").text
   end
-  
-  
-  # def get_courses 
-  
+end
+
+
+   
 #   doc.css(".post").first.css("h2").text
 # => "Web Development Immersive"
-# doc.css(".post").first.css(".date").text
+  #we don't need the doc.css or the .first.css because we are starting the iteration with post 
+# doc.css(".post").first.css(".date").text 
 # => "Full-Time"
 # doc.css(".post").first.css("p").text
 # => "An intensive, Ruby and Javascript course that teaches the skills necessary to start a career as a full-stack software developer."
 
- def make_courses
+
    
-  # def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course| 
-  #     if course.title && course.title != ""
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-   # end 
-  end
+  def print_courses
+    self.make_courses
+    Course.all.each do |course| 
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end 
+end 
 
 Scraper.new.get_page
+
+#QUESTIONS
+
